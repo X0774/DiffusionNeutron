2afbd6409f19074e6bfa8270a5a76187d749ddb1
X0774 <102955347+X0774@users.noreply.github.com>
2022-06-23 19:09:42 +0200

    Update SPHERICALNEUMAN.cxx

--- SPHERICALNEUMAN.cxx
+++ SPHERICALNEUMAN.cxx
@@ -12,7 +12,7 @@
 #include "Numerics.h"
 using namespace std;
 
-double alphamin,alphamax,alpha,eta,mu,lambdat,r1,R;
+double alpha,eta,mu,lambdat,r1,R;
 
 double criticalradius(double r)
 {
@@ -24,6 +24,11 @@ double newalpha (double a)
 	return -1 + 3*R/lambdat/2 + R*sqrt((eta+a)/mu)*cos(R*sqrt((eta+a)/mu))/sin(R*sqrt((eta+a)/mu));
 };
 
+double alphaprime(double r)
+{
+	Numerics num;
+	return num.derivative(newalpha, 0.00001, r);
+};
 
 int main(int argc, char **argv)
 {
@@ -45,12 +50,37 @@ int main(int argc, char **argv)
 	mu = lambdat* v /3; 
 	eta= v *( nu -1) / lambdaf;
 	Numerics num;
-	r1=num.Bisection(criticalradius,0,10,1e-3);
+	r1=num.Bisection(criticalradius,0,10,1e-5);
+	cout << endl;
 	R=8.5;
-	alpha=num.Bisection(newalpha,-2,2e8,1e-3);
-	cout << r1 <<"	"<< alpha;
+	alpha=num.Newton(newalpha,alphaprime, 0, 1e-7);
 	
-	cin >> alpha;
+	//COMPUTING ns
+	double n;
+	cout << "This is now a small test for x and t; please write dx, final t and dt" << endl;
+	double dx,tfin,dt,A;
+	cout << "dx: ";
+	cin >> dx;
+	cout << "t: ";
+	cin >> tfin;
+	cout << "dt: ";
+	cin >> dt;
+	int NPoints=2*abs(R/dx);
+	int NStep=abs(tfin/dt);
+	A=1/(exp(- alpha*0)*sin(sqrt (( eta+ alpha )/mu)*(R*dx))/(R*dx));
+	newfile.open("output.txt",ios::out);
+	newfile << "n(r,t)" << "	" << "t" << "	" << "r" << endl;
+	int c=1;
+	
+	for (int c=1; c<NStep; ++c)
+	{
+		for (int i=1; i<NPoints; ++i)
+		{
+			n=0; //At the end of every loop in x, n must be reset
+			n=A*exp(- alpha *c*dt)*sin(sqrt (( eta+ alpha )/mu)*(-R+i*dx))/(-R+i*dx);
+			newfile << n << "	" << c*dt << "	" << 0+i*dx << endl;	
+		};
+	};
+	newfile.close();	
 	return 0;
 }
-
